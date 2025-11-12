e131aa98754c8f3db6404d6b9b047fff602a430b
gautham-pws <gautham.p@pacewisdom.com>
2025-11-12 21:32:47 +0530

    set up basic api from tutorial

---
+++ app/models/index.js
@@ -0,0 +1,24 @@
+import { config } from '../config/db.config.js';
+import Sequelize from 'sequelize';
+import tutorialModel from './tutorial.model.js';
+
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle
+    }
+})
+
+const db = {}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = tutorialModel(sequelize, Sequelize);
+
+export default db;
--- app/server.js
+++ app/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import db from './models/index.js'
+import tutorialRoutes from './routes/tutorial.routes.js';
 
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+  
 const app = express();
 
 var corsOptions = {
@@ -12,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8080;
+
+tutorialRoutes(app);
 
 app.listen(PORT,()=>{
     console.log("Port is running in: ", PORT)
